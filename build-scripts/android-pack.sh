1c234362cc65868e058879ac6ca5d87acb43c408
Gleefre <varedif.a.s@gmail.com>
2025-06-22 18:14:40 +0200

    Tidy up android-pack.sh

--- build-scripts/android-pack.sh
+++ build-scripts/android-pack.sh
@@ -9,15 +9,8 @@ mkdir -p android-libs
 mkdir -p $DIR/obj
 mkdir -p $DIR/output
 mkdir -p $DIR/src/runtime
-cp -r android-libs $DIR/
-cp src/runtime/sbcl $DIR/src/runtime/
-cp src/runtime/libsbcl.so $DIR/src/runtime/
-cp output/sbcl.core $DIR/output/
-cp -r obj/sbcl-home $DIR/obj/
-cp run-sbcl.sh $DIR/
-cp BUGS $DIR/
-cp NEWS $DIR/
-cp COPYING $DIR/
-cp CREDITS $DIR/
-cp pubring.pgp $DIR/
-cp README $DIR/
+cp -r android-libs $DIR
+cp src/runtime/sbcl src/runtime/libsbcl.so $DIR/src/runtime
+cp output/sbcl.core $DIR/output
+cp -r obj/sbcl-home $DIR/obj
+cp run-sbcl.sh BUGS NEWS COPYING CREDITS pubring.pgp README $DIR
