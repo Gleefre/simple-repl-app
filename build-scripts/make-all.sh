b48c4221963b9d577f339643dc58be45cb39375d
Gleefre <varedif.a.s@gmail.com>
2025-06-23 22:37:11 +0200

    Update make-all.sh

--- build-scripts/make-all.sh
+++ build-scripts/make-all.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
-./build-scripts/copy-sbcl.sh || ./build-scripts/make-sbcl.sh
-./build-scripts/install-quicklisp.sh
-./build-scripts/push-local-projects.sh
+# Lisp environment (sbcl, quicklisp, local-projects)
+./build-scripts/make-sbcl.sh
+./build-scripts/adb-init-quicklisp.sh
+./build-scripts/adb-init-local-projects.sh
+
+# Lisp, C and Java
 ./build-scripts/make-core.sh
 ./build-scripts/make-c.sh
-
 ./gradlew assembleDebug
 
 # cp build/outputs/apk/debug/simple-repl-debug.apk prebuilt-apk/
