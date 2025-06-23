6196d088d0a01f378aa181e34b70ca1a57176789
Gleefre <varedif.a.s@gmail.com>
2025-06-23 22:20:15 +0200

    Use build/external for local-projects

--- build-scripts/adb-init-local-projects.sh
+++ build-scripts/adb-init-local-projects.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -e
 
-if [ ! -d local-projects ]; then
-    mkdir local-projects
+if [ ! -d build/external/local-projects ]; then
+    mkdir -p build/external/local-projects
     (
-        cd ./local-projects ;
-        pwd ;
-        git clone https://github.com/Gleefre/simple-repl.git ;
-        git clone https://github.com/Gleefre/and-jni.git ;
-        git clone https://github.com/Gleefre/cl-android.git ;
-        git clone https://github.com/Gleefre/android-log.git ;
+        cd build/external/local-projects;
+        pwd;
+        git clone https://github.com/Gleefre/simple-repl.git;
+        git clone https://github.com/Gleefre/and-jni.git;
+        git clone https://github.com/Gleefre/cl-android.git;
+        git clone https://github.com/Gleefre/android-log.git;
     )
 fi
 
-adb push local-projects/simple-repl/ /data/local/tmp/quicklisp/local-projects/
-adb push local-projects/and-jni/ /data/local/tmp/quicklisp/local-projects/
-adb push local-projects/android-log/ /data/local/tmp/quicklisp/local-projects/
-adb push local-projects/cl-android/ /data/local/tmp/quicklisp/local-projects/
+adb push build/external/local-projects/simple-repl/ /data/local/tmp/quicklisp/local-projects/
+adb push build/external/local-projects/and-jni/     /data/local/tmp/quicklisp/local-projects/
+adb push build/external/local-projects/cl-android/  /data/local/tmp/quicklisp/local-projects/
+adb push build/external/local-projects/android-log/ /data/local/tmp/quicklisp/local-projects/
