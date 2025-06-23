9be6fbf23b93ec9e5c43f3a4e776d4190eb2bea4
Gleefre <varedif.a.s@gmail.com>
2025-06-23 23:25:30 +0200

    Split out environment and app parts of make-all.sh into different scripts

--- build-scripts/make-all.sh
+++ build-scripts/make-all.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 set -e
 
-# Lisp environment (sbcl, quicklisp, local-projects)
-./build-scripts/make-sbcl.sh
-./build-scripts/adb-init-quicklisp.sh
-./build-scripts/adb-init-local-projects.sh
-
-# Lisp, C and Java
-./build-scripts/make-core.sh
-./build-scripts/make-c.sh
-./build-scripts/make-java.sh  # basically ./gradlew assembleDebug
+./build-scripts/make-env.sh
+./build-scripts/make-app.sh
 
 # cp build/outputs/apk/debug/simple-repl-debug.apk prebuilt-apk/
 # ./gradlew installDebug
