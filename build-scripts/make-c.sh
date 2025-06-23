6f41c2708a33d68c61f1158cf08c3a3d464e7c0b
Gleefre <varedif.a.s@gmail.com>
2025-06-23 22:36:27 +0200

    Update make-c.sh

--- build-scripts/make-c.sh
+++ build-scripts/make-c.sh
@@ -1,21 +1,38 @@
 #!/bin/sh
 set -e
 
-# Guessing architecture.
-if [ -z $ARCH ]; then
-    uname_arch=`adb shell uname -m`
-else
-    uname_arch=$ARCH
+# Check for NDK
+if [ -z $NDK ] || [ ! -d $NDK ]; then
+    echo "Can't find Android NDK, please set the $NDK environment variable."
+    exit 1
 fi
 
-case $uname_arch in
-    x86_64) abi=x86_64 ;;
-    aarch64) abi=arm64-v8a ;;
-    *) echo "Architecture $uname_arch is not supported."
+# Determine target ABI
+if [ -n "$1" ]; then
+    ABI=$1
+elif [ -z "$ABI" ]; then
+    uname_arch=`adb shell uname -m`
+
+    case $uname_arch in
+        x86_64) ABI=x86_64 ;;
+        aarch64) ABI=arm64-v8a ;;
+        *) echo "Architecture $uname_arch is not supported."
+           exit 1 ;;
+    esac
+fi
+echo "Determined target $ABI."
+
+case $ABI in
+    x86_64) TARGET_TAG=x86_64-linux-android ;;
+    arm64-v8a) TARGET_TAG=aarch64-linux-android ;;
+    *) echo "ABI $ABI is not supported."
        exit 1 ;;
 esac
-echo "Architecture $abi determined."
 
-CC=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/$uname_arch-linux-android21-clang
+ANDROID_API=21
+HOST_TAG=linux-x86_64
 
-$CC -fPIC -shared -o libs/$abi/lib.gleefre.wrap.so c/wrap.c -lsbcl -llog -Llibs/$abi
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
+CC=$TOOLCHAIN/bin/$TARGET_TAG$ANDROID_API-clang
+
+$CC -fPIC -shared -o prebuilt/libs/$ABI/lib.gleefre.wrap.so src/c/wrap.c -lsbcl -llog -Lprebuilt/libs/$ABI
