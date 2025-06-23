1282b4fbde3bdc72896e042393d18768aebac46f
Gleefre <varedif.a.s@gmail.com>
2025-06-23 22:11:21 +0200

    Use build/external for downloaded quicklisp.lisp

--- build-scripts/install-quicklisp.sh
+++ build-scripts/install-quicklisp.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-if [ ! -f quicklisp.lisp ]; then
-    wget https://beta.quicklisp.org/quicklisp.lisp
+if [ ! -f build/external/quicklisp.lisp ]; then
+    ( cd build/external;
+      wget https://beta.quicklisp.org/quicklisp.lisp )
 fi
 
-# Clean
+# Clean (adb)
 adb shell rm -rf /data/local/tmp/quicklisp
 adb shell rm -rf /data/local/tmp/quicklisp.lisp
 adb shell rm -rf /data/local/tmp/.slime
 adb shell rm -rf /data/local/tmp/.sbclrc
 adb shell rm -rf /data/local/tmp/.cache/common-lisp
 
-adb push quicklisp.lisp /data/local/tmp
+# Install quicklisp (adb)
+adb push build/external/quicklisp.lisp /data/local/tmp/quicklisp.lisp
 adb shell "cd /data/local/tmp ; export HOME=\$(pwd) ; ./sbcl/run-sbcl.sh --load quicklisp.lisp --eval '(quicklisp-quickstart:install)' --eval '(ql-util:without-prompting (ql:add-to-init-file))' --quit"
-
+adb shell rm -rf /data/local/tmp/quicklisp.lisp
